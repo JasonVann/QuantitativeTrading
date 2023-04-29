32a845e4625c018813e9aec7549b0095038bbe1f
JasonVann <jasonvanet@gmail.com>
2023-04-29 18:48:08 +0800

    Evaluate market by PE

--- src/ak_analysis.py
+++ src/ak_analysis.py
@@ -1,18 +1,111 @@
 import akshare as ak
+import datetime
 
-# 行业数据
-stock_szse_sector_summary_df = ak.stock_szse_sector_summary(symbol="当年", date="202204")
-print(stock_szse_sector_summary_df)
 
-stock_sse_deal_daily_df = ak.stock_sse_deal_daily(date="20201111")
-print(stock_sse_deal_daily_df)
 
-stock_sse_deal_daily_df = ak.stock_sse_deal_daily(date="20211227")
-print(stock_sse_deal_daily_df)
+def eval_market(market = "上证", num_years=10):
+    '''
+    Evaluate PB/PE of the market
+    # "上证", "深证", "创业板", "科创版"
+    :return:
+    '''
+    # 股市PE估值, 每月月底一条数据
+    pe_df = ak.stock_market_pe_lg(market)  # "上证", "深证", "创业板", "科创版"
 
-# stock
-stock_individual_info_em_df = ak.stock_individual_info_em(symbol="000001")
-print(stock_individual_info_em_df)
+    today = datetime.date.today()
+    hist_start = today.replace(today.year - num_years)
 
-stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20170301", end_date='20210907',
-                                        adjust="")
+    df2 = pe_df[pe_df['日期'] > hist_start]
+
+    ak.stock_market_pb_lg("上证")  # "上证", "深证", "创业板", "科创版"
+
+    return get_percentile(df2)
+
+def get_percentile(df):
+    row_data = {}
+    df['rank'] = df['平均市盈率'].rank(pct=True)
+    print(df)
+    temp_df = df.describe()
+
+    temp_df.loc[len(temp_df.index)] = [float(df.tail(1)['指数'].values), float(df.tail(1)['平均市盈率'].values),
+                           float(df.tail(1)['rank'].values)]
+    temp_df = temp_df.rename(index={8: df.tail(1)['日期']})
+
+    return temp_df
+
+
+def eval_sector():
+    '''
+    "sw_index_representation_spot"  # 申万市场表征数据
+    "sw_index_spot"  # 申万一级实时行情
+    "sw_index_second_spot"  # 申万二级实时行情
+    "sw_index_cons"  # 申万一级、二级板块成份
+    "sw_index_daily"  # 申万一级、二级历史行情
+    "sw_index_daily_indicator"  # 申万一级、二级历史行情指标
+    "sw_index_third_info"  # 申万三级信息
+    "sw_index_third_cons"  # 申万三级信息成份
+    "index_level_one_hist_sw"  # 申万指数-指数发布-指数体系-一级行业
+    "index_market_representation_hist_sw"  # 申万指数-指数发布-指数体系-市场表征
+    "index_style_index_hist_sw"  # 申万指数-指数发布-指数体系-风格指数
+    '''
+
+    df = ak.stock_sector_spot('行业')
+    ak.sw_index_daily_indicator()
+
+    # 申万市场表征数据  ??
+    ak.sw_index_representation_spot()
+
+    # 申万一级实时行情
+    ak.sw_index_spot()
+
+    ak.index_level_one_hist_sw('801120')
+
+    ak.stock_sector_detail()
+
+def eval_stock(code='600519'):
+    '''
+    指标: PB, PE, EPS, 夏普比率、最大回撤
+    :param code:
+    :return:
+    '''
+    # 财务指标数据 工行财报
+    df = ak.stock_financial_analysis_indicator(code)
+
+    # 财务摘要
+    df = ak.stock_financial_abstract(code)
+
+    # 三大财务报表
+    df3 = ak.stock_financial_report_sina(code)
+
+    ak.stock_a_lg_indicator
+
+def eval_portfolio():
+    '''
+    指标: PB, PE, EPS, 夏普比率、最大回撤
+    :return:
+    '''
+
+def test_api():
+    stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20170301", end_date='20210907', adjust="")
+    print(stock_zh_a_hist_df)
+
+    # 期货展期收益率
+    get_roll_yield_bar_df = ak.get_roll_yield_bar(type_method="date", var="RB", start_day="20180618", end_day="20180718")
+    print(get_roll_yield_bar_df)
+
+    # 行业数据
+    stock_szse_sector_summary_df = ak.stock_szse_sector_summary(symbol="当年", date="202204")
+    print(stock_szse_sector_summary_df)
+
+    stock_sse_deal_daily_df = ak.stock_sse_deal_daily(date="20201111")
+    print(stock_sse_deal_daily_df)
+
+    stock_sse_deal_daily_df = ak.stock_sse_deal_daily(date="20211227")
+    print(stock_sse_deal_daily_df)
+
+    # stock
+    stock_individual_info_em_df = ak.stock_individual_info_em(symbol="000001")
+    print(stock_individual_info_em_df)
+
+    stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20170301", end_date='20210907',
+                                            adjust="")
---
+++ src/ak_api.md
@@ -0,0 +1,40 @@
+# 申万行业指数
+ "sw_index_representation_spot"  # 申万市场表征数据
+ "sw_index_spot"  # 申万一级实时行情
+ "sw_index_second_spot"  # 申万二级实时行情
+ "sw_index_cons"  # 申万一级、二级板块成份
+ "sw_index_daily"  # 申万一级、二级历史行情
+ "sw_index_daily_indicator"  # 申万一级、二级历史行情指标
+ "sw_index_third_info"  # 申万三级信息
+ "sw_index_third_cons"  # 申万三级信息成份
+ "index_level_one_hist_sw"  # 申万指数-指数发布-指数体系-一级行业
+ "index_market_representation_hist_sw"  # 申万指数-指数发布-指数体系-市场表征
+ "index_style_index_hist_sw"  # 申万指数-指数发布-指数体系-风格指数
+
+ # 股票基本面数据
+ "stock_financial_abstract"  # 财务摘要
+ "stock_financial_report_sina"  # 三大财务报表
+ "stock_financial_analysis_indicator"  # 财务指标
+ "stock_add_stock"  # 股票增发
+ "stock_ipo_info"  # 股票新股
+ "stock_history_dividend_detail"  # 分红配股
+ "stock_history_dividend"  # 历史分红
+ "stock_dividents_cninfo"  # 个股历史分红
+ "stock_restricted_release_queue_sina"  # 限售解禁-新浪
+ "stock_restricted_release_summary_em"  # 东方财富网-数据中心-特色数据-限售股解禁
+ "stock_restricted_release_detail_em"  # 东方财富网-数据中心-限售股解禁-解禁详情一览
+ "stock_restricted_release_queue_em"  # 东方财富网-数据中心-个股限售解禁-解禁批次
+ "stock_restricted_release_stockholder_em"  # 东方财富网-数据中心-个股限售解禁-解禁股东
+ "stock_circulate_stock_holder"  # 流动股东
+ "stock_fund_stock_holder"  # 基金持股
+ "stock_main_stock_holder"  # 主要股东
+
+ # A 股市盈率和市净率
+ "stock_market_pe_lg"  # 乐咕乐股-主板市盈率
+ "stock_index_pe_lg"  # 乐咕乐股-指数市盈率
+ "stock_market_pb_lg"  # 乐咕乐股-主板市净率
+ "stock_index_pb_lg"  # 乐咕乐股-指数市净率
+ "stock_a_indicator_lg"  # A 股个股市盈率、市净率和股息率指标
+ "stock_hk_indicator_eniu"  # 港股股个股市盈率、市净率和股息率指标
+ "stock_a_high_low_statistics"  # 创新高和新低的股票数量
+ "stock_a_below_net_asset_statistics"  # 破净股统计
